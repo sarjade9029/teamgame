ba93c10fd4092f57617a45b2d92eacf717f60548
jade9029 <ds215323@gmail.com>
2019-05-28 15:05:48 +0900

    無視リストからassetの除外とプレイヤーのスクリプト作成

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public bool onTheWall = false;
+    public int stamina = 10;
+    public int sleepiness = 0;
+    public float speed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody myRigid = GetComponent<Rigidbody>();
+        if(onTheWall == false)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                myRigid.AddForce(new Vector3(0, speed, 0));
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRigid.AddForce(new Vector3(speed, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRigid.AddForce(new Vector3(-speed, 0, 0));
+        }
+        if(onTheWall == true)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                myRigid.AddForce(new Vector3(0, 0, -speed));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                myRigid.AddForce(new Vector3(0, 0, speed));
+            }
+        }
+
+    }
+}
---
+++ Assets/hide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour
+{
+    public GameObject gameobject;
+    //1.0fで描画0.0fで非表示
+    float changeRed = 1.0f;
+    float changeGreen = 1.0f;
+    float cahngeBlue = 1.0f;
+    float cahngeAlpha = 1.0f;
+    // Use this for initialization
+    void Start()
+    {
+        changeRed = 1.0f;
+        changeGreen = 1.0f;
+        cahngeBlue = 1.0f;
+        cahngeAlpha = 1.0f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent().color = new Color(changeRed, changeGreen, cahngeBlue, cahngeAlpha);
+    }
+}
