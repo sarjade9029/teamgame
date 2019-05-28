eb368c3c5b387cc6c311d41ad832a58248ffb976
翡翠 <ds215323@gmail.com>
2019-05-28 16:28:46 +0900

    プレイヤーのスクリプト作成続き

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -7,38 +7,78 @@ public class PlayerMove : MonoBehaviour
     public bool onTheWall = false;
     public int stamina = 10;
     public int sleepiness = 0;
-    public float speed = 10.0f;
+    public float normalSpeed = 10.0f;
+    public int Cooltime = 5;
+    public int witecount = 0;
+    float speed = 0.0f;
+    float fatigue = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
-        Rigidbody myRigid = GetComponent<Rigidbody>();
-        if(onTheWall == false)
+
+        SpeedCalculator();
+
+        if (Cooltime == 0)
         {
-            if (Input.GetKey(KeyCode.Space))
+            if (stamina < 10)
             {
-                myRigid.AddForce(new Vector3(0, speed, 0));
+                stamina++;
             }
         }
-        if (Input.GetKey(KeyCode.A))
+        InputMove();
+    }
+
+    void SpeedCalculator()
+    {
+        if (stamina == 10)
+        {
+            fatigue = 1.0f;
+        }
+        if (stamina <= 7)
+        {
+            fatigue = 0.9f;
+        }
+        if (stamina <= 3)
+        {
+            fatigue = 0.7f;
+        }
+        if (stamina == 1)
+        {
+            fatigue = 0.5f;
+        }
+
+        speed = normalSpeed * fatigue;
+    }
+
+    void InputMove()
+    {
+        Rigidbody myRigid = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Keypad4))
         {
             myRigid.AddForce(new Vector3(speed, 0, 0));
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Keypad6))
         {
             myRigid.AddForce(new Vector3(-speed, 0, 0));
         }
-        if(onTheWall == true)
+        if (onTheWall == true)
         {
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Keypad8))
             {
                 myRigid.AddForce(new Vector3(0, 0, -speed));
             }
-            if (Input.GetKey(KeyCode.S))
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.Keypad2))
             {
                 myRigid.AddForce(new Vector3(0, 0, speed));
             }
         }
-
+        else
+        {
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Keypad8))
+            {
+                myRigid.AddForce(new Vector3(0, speed, 0));
+            }
+        }
     }
 }
