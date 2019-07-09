23998451c22590ed7c81458eb3708df4657b91ac
花田　空 <ds215323@gmail.com>
2019-07-09 19:54:07 +0900

    壁で歩く時と地上を歩く時の位置を共有

--- Assets/Othergraph.cs
+++ Assets/Othergraph.cs
@@ -8,15 +8,31 @@ public class Othergraph : MonoBehaviour
     private float graphB = 0.0f;
     private float hidden = 0.0f;
     private float display = 1.0f;
+    private bool hitA = true;
+    private bool hitB = false;
     // Start is called before the first frame update
     SpriteRenderer spriteRenderer;
     GameObject other;
     SpriteRenderer otherSpriteRenderer;
+    SpriteRenderer other2SpriteRenderer;
     void Start()
     {
-        other = GameObject.Find("");
+        //べつの状態のプレイヤーとその子オブジェクトの取得
+        other = GameObject.Find("player");
         otherSpriteRenderer = other.GetComponent<SpriteRenderer>();
-        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        Transform other1 = other.transform.Find("otherCollider");
+        Transform other2 = other.transform.Find("playerline");
+        other2SpriteRenderer = other2.GetComponent<SpriteRenderer>();
+        //プレイヤー本体の初期化
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(1, 1, 1, graphA);
+        GetComponent<PolygonCollider2D>().enabled = hitA;
+        //別の状態のプレイヤーの初期化
+        otherSpriteRenderer.color = new Color(1, 1, 1, graphB);
+        other.GetComponent<PolygonCollider2D>().enabled = hitB;
+        //別の状態の子オブジェクトの初期化
+        other2SpriteRenderer.color = new Color(1, 1, 1, graphB);
+        other1.GetComponent<BoxCollider2D>().enabled = hitB;
     }
 
     // Update is called once per frame
---
+++ Assets/Setpos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setpos : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject wallplayer;
+    // Update is called once per frame
+    void Update()
+    {
+        wallplayer.transform.position = player.transform.position;
+    }
+}
