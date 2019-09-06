49bb291cabab305155e020175d4e9d42eae9414e
花田　空 <ds215323@gmail.com>
2019-09-06 18:06:12 +0900

    ボタンと鍵の表示非表示のスクリプト追加

---
+++ Assets/Brightness.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Brightness : MonoBehaviour
+{
+    public GameObject buttonB;
+    public GameObject buttonY;
+    public GameObject buttonRb;
+    public GameObject Key;
+    Image B;
+    Image Y;
+    Image RB;
+    Image Keyimage;
+    public float red;
+    public float gree;
+    public float blue;
+    public float alpha;
+    public float foll = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      B = buttonB.GetComponent<Image>();
+      Y = buttonY.GetComponent<Image>();
+      RB = buttonRb.GetComponent<Image>();
+      Keyimage = Key.GetComponent<Image>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        B.color = new Color(red, gree, blue, alpha);
+        Y.color = new Color(red, gree, blue, alpha);
+        RB.color = new Color(red, gree, blue, alpha);
+        Keyimage.color = new Color(red, gree, blue, alpha);
+    }
+}
