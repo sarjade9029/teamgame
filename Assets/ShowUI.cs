2d003711a52b26b8b097f98955acb60ac0d36968
jade9029 <ds215323@gmail.com>
2019-09-10 11:21:57 +0900

    文字と表示を別々にする

--- Assets/ShowUI.cs
+++ Assets/ShowUI.cs
@@ -7,19 +7,10 @@ public class ShowUI : MonoBehaviour
 {
     public GameObject flame;
     public GameObject text;
-    public string Text;
     Image flameimage;
     Text ui_text;
-    public int textnum = 0;
     private float alpha = 1;
     private float zero = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        flameimage = flame.GetComponent<Image>();
-        ui_text = text.GetComponent<Text>();
-        ui_text.text = Text;
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         flameimage.color = new Color(1, 1, 1, alpha);
